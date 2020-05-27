3398780794e47ca27d7a1a59a094747d78a25551
Maxym Martynenkow <martymaxon@gmail.com>
2020-05-27 20:30:29 +0300

    styles.php

--- styles.php
+++ styles.php
@@ -1,2 +1,4 @@
 <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="node_modules/cropperjs/dist/cropper.min.css">
+<link href="node_modules/font-awesome/css/font-awesome.min.css" rel="stylesheet">
